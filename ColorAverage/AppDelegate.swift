107eb6c5545c5b000c857699ddac1f9341accfe8
Teo Sartori <m@sartori.dk>
2016-06-05 19:19:00 +0200

    Changed syntax to Swift 3. Drag & drop broken.

--- ColorAverage/AppDelegate.swift
+++ ColorAverage/AppDelegate.swift
@@ -49,15 +49,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
         guard let avgFilter = CIFilter(name: "CIAreaAverage") else { return nil }
         avgFilter.setValue(inputImage, forKey: kCIInputImageKey)
         let imageRect = inputImage.extent
-        avgFilter.setValue(CIVector(CGRect: imageRect), forKey: kCIInputExtentKey)
+        avgFilter.setValue(CIVector(cgRect: imageRect), forKey: kCIInputExtentKey)
         
-        let newImage = NSImageFromCIImage(avgFilter.outputImage!)
-        let newCol = extractColor(newImage)
+        let newImage = NSImageFromCIImage(theCIImage: avgFilter.outputImage!)
+        let newCol = extractColor(theImage: newImage)
         return newCol
     }
     
     func NSImageFromCIImage(theCIImage: CIImage) -> NSImage {
-        let rep = NSCIImageRep(CIImage:theCIImage)
+        let rep = NSCIImageRep(ciImage:theCIImage)
         let newImage = NSImage(size: rep.size)
         newImage.addRepresentation(rep)
         return newImage
@@ -65,7 +65,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
     
     func CIImageFromNSImage(inputImage: NSImage) ->CIImage? {
         if let
-            imageData = inputImage.TIFFRepresentation,
+            imageData = inputImage.tiffRepresentation,
             bitmap = NSBitmapImageRep(data: imageData) {
             return CIImage(bitmapImageRep: bitmap)
         }
@@ -74,14 +74,14 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
     
     func extractColor(theImage: NSImage) -> NSColor? {
         var pixel = Pixel(red: 0, green: 0, blue: 0, alpha: 0)
-        if let imageData = theImage.TIFFRepresentation {
-            guard let source = CGImageSourceCreateWithData(imageData as CFDataRef, nil) else { return nil }
+        if let imageData = theImage.tiffRepresentation {
+            guard let source = CGImageSourceCreateWithData(imageData as CFData, nil) else { return nil }
             let maskRef = CGImageSourceCreateImageAtIndex(source, 0, nil)
             let colorSpace = CGColorSpaceCreateDeviceRGB()
 //            var bitmapInfo = CGBitmapInfo.ByteOrder32Big.union(CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedLast.rawValue))
-            let context = CGBitmapContextCreate(&pixel, 1, 1, 8, 4, colorSpace, CGImageAlphaInfo.PremultipliedLast.rawValue)
+            let context = CGContext(data: &pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)
             
-            CGContextDrawImage(context, CGRectMake(0, 0, 1, 1),maskRef)
+            context?.draw(in: CGRect(x: 0, y: 0,width: 1,height: 1),image: maskRef)
 
             let r = CGFloat(pixel.red) / CGFloat(255.0)
             let g = CGFloat(pixel.green) / CGFloat(255.0)
@@ -94,7 +94,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
     
     func extractColorFromCIImage(theImage: CIImage) ->NSColor {
         var pixel = Pixel(red: 0, green: 0, blue: 0, alpha: 0)
-        let curCIContext = NSGraphicsContext.currentContext()?.CIContext
+        let curCIContext = NSGraphicsContext.current()?.ciContext
         //let extent = theImage.extent
         curCIContext?.render(theImage, toBitmap: &pixel, rowBytes: 4, bounds: theImage.extent , format: kCIFormatARGB8, colorSpace: CGColorSpaceCreateDeviceRGB())
         
@@ -109,7 +109,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
         let theImage = NSImage(size: theSize)
         theImage.lockFocus()
         theColor.setFill()
-        NSBezierPath.fillRect(NSMakeRect(0, 0, theSize.width, theSize.height))
+        NSBezierPath.fill(NSRect(x: 0, y: 0, width: theSize.width, height: theSize.height))
         theImage.unlockFocus()
         return theImage
     }
@@ -124,11 +124,11 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
     }
     
     func dropViewDidReceiveURL(theURL: NSURL) {
-        guard let startCIImage = CIImage(contentsOfURL: theURL) else { return }
-        beforeImage.image = NSImageFromCIImage(startCIImage)
-        beforeLabel.hidden = false
+        guard let startCIImage = CIImage(contentsOf: theURL) else { return }
+        beforeImage.image = NSImageFromCIImage(theCIImage: startCIImage)
+        beforeLabel.isHidden = false
         
-        if let avgCol = fastColorAvg(startCIImage) {
+        if let avgCol = fastColorAvg(inputImage: startCIImage) {
             let complement = NSColor(calibratedRed: 1-avgCol.redComponent, green: 1-avgCol.greenComponent, blue: 1-avgCol.blueComponent, alpha: avgCol.alphaComponent)
 
             redLabel.stringValue = NSString(format:"0x%2x",UInt(avgCol.redComponent*255)) as String
@@ -141,20 +141,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, DropViewDelegate {
             //window.backgroundColor = complement
             let imageSize = startCIImage.extent.size
 
-            let invImage = makeSingleColorImage(complement, theSize: imageSize)
-            guard let correctedCIImage = applyOverlayFilter(startCIImage, invColorAvgImage: CIImageFromNSImage(invImage)!) else { return }
+            let invImage = makeSingleColorImage(theColor: complement, theSize: imageSize)
+            guard let correctedCIImage = applyOverlayFilter(theImage: startCIImage, invColorAvgImage: CIImageFromNSImage(inputImage: invImage)!) else { return }
             
-            afterImage.image = NSImageFromCIImage(correctedCIImage)// anImage
-            afterLabel.hidden = false
+            afterImage.image = NSImageFromCIImage(theCIImage: correctedCIImage)// anImage
+            afterLabel.isHidden = false
             
-            saveCIImageAsPNG(correctedCIImage, toPath: "/Users/teo/tmp/notred.png")
+            saveCIImageAsPNG(theImage: correctedCIImage, toPath: "/Users/teo/tmp/notred.png")
         }
     }
     
     func saveCIImageAsPNG(theImage: CIImage, toPath: String) {
-        let bitmap = NSBitmapImageRep(CIImage: theImage) as NSBitmapImageRep
-        let pngData = bitmap.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])
-        pngData?.writeToFile(toPath, atomically:false)
+        let bitmap = NSBitmapImageRep(ciImage: theImage) as NSBitmapImageRep
+        let pngData = bitmap.representation(using: NSBitmapImageFileType.NSPNGFileType, properties: [:])
+        pngData?.write(toFile: toPath, atomically:false)
     }
 }
 
@@ -168,7 +168,7 @@ class DragView: NSView {
     override init(frame: NSRect) {
         super.init(frame: frame)
         let types = [NSFilenamesPboardType]
-        registerForDraggedTypes(types)
+        register(forDraggedTypes: types)
     }
     
 
@@ -176,17 +176,17 @@ class DragView: NSView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func draggingEntered(sender: NSDraggingInfo) -> NSDragOperation {
-        return NSDragOperation.Copy
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        return NSDragOperation.copy
     }
     
-    override func performDragOperation(sender: NSDraggingInfo) -> Bool {
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
         let pboard = sender.draggingPasteboard()
         let myArray = pboard.types! as NSArray
         
-        if myArray.containsObject(NSURLPboardType),
-            let fileURL = NSURL(fromPasteboard: pboard) {
-                delegate?.dropViewDidReceiveURL(fileURL)
+        if myArray.contains(NSURLPboardType),
+            let fileURL = NSURL(from: pboard) {
+                delegate?.dropViewDidReceiveURL(theURL: fileURL)
         }
         return true
     }
